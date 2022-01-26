d2f5c480ff6cc079961d3b4753609e7c8d818c26
wilquey <wilquey@gmail.com>
2022-01-26 20:01:34 -0300

    Second commit

---
+++ app/Models/Plan.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Plan extends Model
+{
+    protected $fillable = [
+        'name',
+        'url',
+        'price',
+        'description',
+    ];
+
+    public function search($filter = null)
+    {
+        $results = $this
+                    ->where('name', 'LIKE', "%{$filter}%")
+                    ->orWhere('description', 'LIKE', "%{$filter}%")
+                    ->paginate();
+
+        return $results;
+    }
+
+}
+
+// settings.json - anterior ;;;... agora está como         "editor.autoClosingBrackets": "always"
+// "editor.defaultFormatter": "shufo.vscode-blade-formatter"
+
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -10,6 +13,17 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('admin/plans/create', 'Admin\PlanController@create')->name('plans.create');
+Route::put('admin/plans/{url}', 'Admin\PlanController@update')->name('plans.update');
+Route::get('admin/plans/{url}/edit', 'Admin\PlanController@edit')->name('plans.edit');
+Route::any('admin/plans/search', 'Admin\PlanController@search')->name('plans.search');
+Route::delete('admin/plans/{url}', 'Admin\PlanController@destroy')->name('plans.destroy');
+Route::get('admin/plans/{url}', 'Admin\PlanController@show')->name('plans.show');
+Route::post('admin/plans/store', 'Admin\PlanController@store')->name('plans.store');
+Route::get('admin/plans', 'Admin\PlanController@index')->name('plans.index');
+
+Route::get('admin', 'Admin\PlanController@index')->name('admin.index');
 
 Route::get('/', function () {
     return view('welcome');
